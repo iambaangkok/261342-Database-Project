18e2e74eb7bf2a4be7b2b2ec6621103eeab383ad
gigiftttgi <76035585+gigiftttgi@users.noreply.github.com>
2022-10-28 16:21:32 +0700

    update user table

--- database/migrations/2022_10_12_000001_create_users_table.php
+++ database/migrations/2022_10_12_000001_create_users_table.php
@@ -17,13 +17,12 @@ return new class extends Migration
             $table->integer('id')->autoIncrement();
             $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->integer('customerNumber')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
-
-        
     }
 
     /**
