64082893ee2ef9ff04d95c057f47ef2584a1ee4a
Paint <paintlpch@gmail.com>
2022-10-28 12:26:16 +0700

    login response with token

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,19 +14,21 @@ class LoginController extends Controller
         /** * Handle account login request * * @param LoginRequest $request * * @return \Illuminate\Http\Response  */
     
     public function login(LoginRequest $request) {
+            
             $credentials = $request->getCredentials();
+            
             if(!Auth::validate($credentials)):
-                return redirect()->to('login') ->withErrors(trans('auth.failed'));
+                return response()->json(['success'=>'false','message' => 'Login Failed.'], 422);
                 endif; 
                 $user = Auth::getProvider()->retrieveByCredentials($credentials);
-                Auth::login($user);
-                return $this->authenticated($request, $user);
+                Auth::login($user, $remember = true);
+                $token = $user['remember_token'];
+                return response()->json($token, 200);
         } 
         
         /** * Handle response after user authenticated * * @param Request $request * @param Auth $user * * @return \Illuminate\Http\Response */
         protected function authenticated(Request $request, $user) 
         {
-            return redirect()->intended();
+            return response()->json($user, 200);
         }
 }
-
