d92c2695bcc6a67a7e726580efc59417bf638679
Nonthawt20 <Nonthawt20@gmail.com>
2022-11-06 14:40:49 +0700

    เชื่อมตาราง  User  กับ Customer

---
+++ database/migrations/2022_10_20_172322_add_foreign_keys_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeysToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign(['customerNumber'], 'customer_ibfk_1')->references(['customerNumber'])->on('customer');
+            // $table->foreign(['productCode'], 'cart_ibfk_2')->references(['productCode'])->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropForeign('customer_ibfk_1');
+            // $table->dropForeign('cart_ibfk_2');
+        });
+    }
+}
