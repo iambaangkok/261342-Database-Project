7dbba25518522c43cf3162da2da06134bbde59ef
Nonthawt20 <Nonthawt20@gmail.com>
2022-11-06 15:10:25 +0700

    page Order

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class OrderController extends Controller
+{
+    public function pagination(Request $request)
+    {
+        $remember_token = $request["remember_token"];
+        
+        $user = User::where('remember_token', '=' , $remember_token)->first();
+        $customer = Customer::where('customerNumber', '=', $user->customerNumber)->first();
+        $page = Order::where('customerNumber', '=',$customer->customerNumber)
+                    ->paginate(15);
+        ;
+
+        return $page;
+
+    }
+}
